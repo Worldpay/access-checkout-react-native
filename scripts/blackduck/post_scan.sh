997f3777b7a45dc96aee54fc3442f370d2059e1e
Vishal Jethwa <vishal.jethwa@fisglobal.com>
2021-12-10 16:24:39 +0000

    US739202: update payload

--- scripts/blackduck/post_scan.sh
+++ scripts/blackduck/post_scan.sh
@@ -6,19 +6,19 @@ if [ $CODEBUILD_BUILD_SUCCEEDING -eq 1 ]; then
   emoji=":duck:"
   color="#31AD72"
   header="Success"
-  text="Blackduck Scan for Access Checkout build *No.${CODEBUILD_BUILD_NUMBER}* for *${CODEBUILD_SOURCE_VERSION}* for version *${SDK_VERSION}* has completed successfully."
-  location="Report can be retrieved from *GW2 Dev* here: https://s3.console.aws.amazon.com/s3/object/wp-hydra-artefact-bucket?region=eu-west-1&prefix=access-checkout-web-blackduck/${CODEBUILD_BUILD_NUMBER}/Access_Checkout_Web_30000270_${REPORT_SDK_VERSION}_BlackDuck_RiskReport.pdf"
+  text="Blackduck Scan for Access Checkout React Native build *No.${CODEBUILD_BUILD_NUMBER}* for *${CODEBUILD_SOURCE_VERSION}* for version *${SDK_VERSION}* has completed successfully."
+  location="Report can be retrieved from *GW2 Dev* here: https://s3.console.aws.amazon.com/s3/object/wp-hydra-artefact-bucket?region=eu-west-1&prefix=access-checkout-react-native-blackduck/${CODEBUILD_BUILD_NUMBER}/Access_Checkout_React_Native_30000270_${REPORT_SDK_VERSION}_BlackDuck_RiskReport.pdf"
 else
   emoji=":duck:"
   color="#EA5E1A"
   header="Failed"
-  text="Blackduck Scan for Access Checkout build *No.${CODEBUILD_BUILD_NUMBER}* for *${CODEBUILD_SOURCE_VERSION}* for version *${SDK_VERSION}* has failed."
+  text="Blackduck Scan for Access Checkout React Native build *No.${CODEBUILD_BUILD_NUMBER}* for *${CODEBUILD_SOURCE_VERSION}* for version *${SDK_VERSION}* has failed."
   location="No report generated"
 fi
 
 payload="{
 	\"channel\":\"#hydra-blackduck\",
-	\"username\":\"Access Checkout Blackduck Scan\",
+	\"username\":\"Access Checkout React Native Blackduck Scan\",
 	\"icon_emoji\": \"${emoji}\",
 	\"attachments\": [
 		{
