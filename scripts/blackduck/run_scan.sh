5fcfe61ce429567683c6edb244cbe97160213376
Vishal Jethwa <vishal.jethwa@fisglobal.com>
2021-12-10 16:18:34 +0000

    US739202: updated argument

--- scripts/blackduck/run_scan.sh
+++ scripts/blackduck/run_scan.sh
@@ -6,4 +6,4 @@ cd access-checkout-react-native-sdk
 curl -LOk https://detect.synopsys.com/detect.sh
 chmod +x ./detect.sh
 SDK_VERSION=$(sed -e 's/^"//' -e 's/"$//' <<< $(jq '.version' package.json))
-./detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME --detect.project.version.name=$SDK_VERSION --detect.risk.report.pdf=true --detect.npm.include.dev.dependencies=false --detect.detector.search.exclusion.paths=android,ios
+./detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME --detect.project.version.name=$SDK_VERSION --detect.risk.report.pdf=true --detect.npm.include.dev.dependencies=false --detect.excluded.directories=android,ios
