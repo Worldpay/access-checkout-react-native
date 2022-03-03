98d49e0e1d31e571243f6403025c8bf7414f76e7
Siân <sian.kearley@fisglobal.com>
2022-03-03 17:20:37 +0000

    US889633: amend publish script to make it more robust

--- access-checkout-react-native-sdk/scripts/publish.sh
+++ access-checkout-react-native-sdk/scripts/publish.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-version="0.0.1"
-mavenLocalRepository=~/.m2/repository/com/worldpay/access/access-checkout-react-native-sdk-android-bridge/$version
+version="$(jq -r '.version' ./package.json)"
+if ! [[ "${version}" =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)$ ]]; then
+  echo "Failed to find version in format x.y.z (instead found '${version}')"
+  exit 1
+fi
+
+mavenLocalPublicationOutputPath=~/.m2/repository/com/worldpay/access/access-checkout-react-native-sdk-android-bridge/$version
+registryAddress="http://localhost:4873"
 
 cd android
 echo "Deleting Android Bridge Artifacts from Local .m2 directory"
-rm -Rf publish $mavenLocalRepository/*
+rm -Rf publish $mavenLocalPublicationOutputPath/*
 if [ $? -ne 0 ]; then
   echo "Failed to delete Android Bridge Artifacts from Local .m2 directory"
   exit 1
@@ -24,12 +30,38 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
+androidBridgeAarPath="${mavenLocalPublicationOutputPath}/access-checkout-react-native-sdk-android-bridge-${version}.aar"
+echo $androidBridgeAarPath
+if ! [[ -f "${androidBridgeAarPath}" ]]; then
+    echo "Failed to find Android Bridge aar file at ${androidBridgeAarPath}"
+    exit 1
+fi
+
+androidBridgePomPath="${mavenLocalPublicationOutputPath}/access-checkout-react-native-sdk-android-bridge-${version}.pom"
+if ! [[ -f "${androidBridgePomPath}" ]]; then
+    echo "Failed to find Android Bridge pom file at ${androidBridgePomPath}"
+    exit 1
+fi
+
+androidBridgeModulePath="${mavenLocalPublicationOutputPath}/access-checkout-react-native-sdk-android-bridge-${version}.module"
+if ! [[ -f "${androidBridgeModulePath}" ]]; then
+    echo "Failed to find Android Bridge module file at ${androidBridgeModulePath}"
+    exit 1
+fi
+
 echo "Move the following files from local .m2 directory into the newly created 'android/publish/' folder"
-ls -1 $mavenLocalRepository
-mv $mavenLocalRepository/* ./publish
+ls -1 $mavenLocalPublicationOutputPath
+mv $mavenLocalPublicationOutputPath/* ./publish
 if [ $? -ne 0 ]; then
   echo "Failed to move the following files from local .m2 directory into the newly created 'android/publish/' folder"
   exit 1
 fi
 
+echo "Publish React Native SDK ${version} to ${registryAddress}"
+cd ..
+npm publish --registry $registryAddress
+if [ $? -ne 0 ]; then
+  echo "Failed publish React Native SDK"
+  exit 1
+fi
 
