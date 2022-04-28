1dd908da034c96c54c538c3c1d4584675c47ebb9
Olivier Chalet <olivier.chalet@worldpay.com>
2022-04-28 10:40:56 +0100

    US799387: change demo app to target npe env instead of preprod

--- demo-app/e2e/helpers/RegularExpressions.js
+++ demo-app/e2e/helpers/RegularExpressions.js
@@ -1,6 +1,6 @@
 const verifiedTokensSessionRegEx =
-  /https:\/\/preprod\.access\.worldpay\.com\/verifiedTokens\/sessions\/[a-zA-Z0-9-]+/;
+  /https:\/\/npe\.access\.worldpay\.com\/verifiedTokens\/sessions\/[a-zA-Z0-9-]+/;
 const cvcSessionRegEx =
-  /https:\/\/preprod\.access\.worldpay\.com\/sessions\/[a-zA-Z0-9-]+/;
+  /https:\/\/npe\.access\.worldpay\.com\/sessions\/[a-zA-Z0-9-]+/;
 
 module.exports = { verifiedTokensSessionRegEx, cvcSessionRegEx };
