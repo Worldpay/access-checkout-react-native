f66471b61aad02adacf45daa41232fa751fd8a04
Siân <sian.kearley@fisglobal.com>
2022-04-28 20:08:32 +0100

    US786390: Create CvcFlow.tsx (copy of CardFlow.tsx for testing purposes) and added ReactNative navigation to App.tsx

--- demo-app/src/App.tsx
+++ demo-app/src/App.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CardFlow from './flows/CardFlow';
+import CvcFlow from './flows/CvcFlow';
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Card Flow" component={CardFlow} />
+      <Tab.Screen name="Cvc Flow" component={CvcFlow} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
-  return <CardFlow />;
+  return (
+    <React.Fragment>
+      <CardFlow />
+      <CvcFlow />
+      <NavigationContainer>
+        <MyTabs />
+      </NavigationContainer>
+    </React.Fragment>
+  );
 }
