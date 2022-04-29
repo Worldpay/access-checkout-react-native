75fbe1d222a77d82bf0114ed0f623cfddce7bf8e
Olivier Chalet <olivier.chalet@worldpay.com>
2022-04-29 17:27:45 +0100

    US799387: change CI pipeline to run Detox e2e tests in trace log level

--- demo-app/scripts/ci/detox-android-run.sh
+++ demo-app/scripts/ci/detox-android-run.sh
@@ -10,7 +10,7 @@ echo "Running Detox tests"
 
 # We do not use the --cleanup option because Detox already stops the Android device at the end of the tests in BitRise
 # Passing the --cleanup flag causes an error where Detox attempts to stop the device twice hence failing the test task
-JEST_HTML_REPORTER_OUTPUT_PATH="./reports/e2e-tests/android-e2e-tests.html" detox test --configuration android-ci --loglevel verbose
+JEST_HTML_REPORTER_OUTPUT_PATH="./reports/e2e-tests/android-e2e-tests.html" detox test --configuration android-ci --loglevel trace
 status=$?
 
 ./scripts/stop-react-native-background.sh
