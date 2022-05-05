ae2d66b1813f2bcac03b0731a162215dfb2177b6
Olivier Chalet <olivier.chalet@worldpay.com>
2022-05-05 14:07:38 +0100

    US786390: add navigation for Cvc only flow

--- demo-app/src/App.tsx
+++ demo-app/src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import CardFlow from './flows/CardFlow';
-import CvcFlow from './flows/CvcFlow';
-
-const Tab = createBottomTabNavigator();
-
-function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Card Flow" component={CardFlow} />
-      <Tab.Screen name="Cvc Flow2" component={CvcFlow} />
-    </Tab.Navigator>
-  );
-}
+import React, { useState } from 'react';
+import styles from './App-style';
+import CardFlow from './card-flow/CardFlow';
+import HView from './common/HView';
+import VView from './common/VView';
+import CvcOnlyFlow from './CvcOnly';
+import NavItem from './navigation/NavItem';
 
 export default function App() {
+  const screens = {
+    card: 'card',
+    cvc: 'cvc',
+  };
+
+  const [screen, setScreen] = useState(screens.card);
+
   return (
-    <React.Fragment>
-      <NavigationContainer>
-        <MyTabs />
-      </NavigationContainer>
-    </React.Fragment>
+    <VView style={styles.app}>
+      <VView style={styles.main}>
+        {(screen === screens.card) ? <CardFlow /> : <CvcOnlyFlow />}
+      </VView>
+      <HView style={styles.nav}>
+        <NavItem
+          title="Card flow"
+          image={screens.card}
+          selected={screen === screens.card}
+          onPress={() => setScreen(screens.card)}
+          style={{ marginRight: 50 }}
+        />
+        <NavItem
+          title="Cvc only flow"
+          image={screens.cvc}
+          selected={screen === screens.cvc}
+          onPress={() => setScreen(screens.cvc)}
+        >
+        </NavItem>
+      </HView>
+    </VView>
   );
 }
