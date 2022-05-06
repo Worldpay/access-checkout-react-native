a0f380c78bf736615eaf4d0a408acea65a1c7521
Olivier Chalet <olivier.chalet@worldpay.com>
2022-05-06 12:32:24 +0100

    US000000: add git hook to check commit message formats

--- scripts/githooks/install.sh
+++ scripts/githooks/install.sh
@@ -4,3 +4,8 @@
 echo "#!/bin/bash
 ./scripts/githooks/pre-commit" > .git/hooks/pre-commit
 chmod 755 .git/hooks/pre-commit
+
+# commit-msg hook
+echo "#!/bin/bash
+./scripts/githooks/commit-msg" > .git/hooks/commit-msg
+chmod 755 .git/hooks/commit-msg
