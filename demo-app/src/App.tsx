7973240dd8158c8099da4efb43075a947137377b
Siân <sian.kearley@fisglobal.com>
2022-05-06 16:21:20 +0100

    US786390: adding cvc only flow generateSessions

--- demo-app/src/App.tsx
+++ demo-app/src/App.tsx
@@ -3,7 +3,7 @@ import styles from './App-style';
 import CardFlow from './card-flow/CardFlow';
 import HView from './common/HView';
 import VView from './common/VView';
-import CvcOnlyFlow from './CvcOnly';
+import CvcOnly from './cvc-flow/CvcFlow';
 import NavItem from './navigation/NavItem';
 
 export default function App() {
@@ -17,18 +17,18 @@ export default function App() {
   return (
     <VView style={styles.app}>
       <VView style={styles.main}>
-        {screen === screens.card ? <CardFlow /> : <CvcOnlyFlow />}
+        {screen === screens.card ? <CardFlow /> : <CvcOnly />}
       </VView>
       <HView style={styles.nav}>
         <NavItem
-          title="Card flow"
+          title="Card Flow"
           image={screens.card}
           selected={screen === screens.card}
           onPress={() => setScreen(screens.card)}
           style={{ marginRight: 50 }}
         />
         <NavItem
-          title="Cvc only flow"
+          title="Cvc Only Flow"
           image={screens.cvc}
           selected={screen === screens.cvc}
           onPress={() => setScreen(screens.cvc)}
