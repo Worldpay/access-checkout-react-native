cdb709846c505493ea49869a6712c8af225d3277
Olivier Chalet <olivier.chalet@fisglobal.com>
2023-02-01 11:30:29 +0000

    US1376930: attempt to fix issue to run e2e tests for iOS demo app

--- demo-app/scripts/ci/detox-ios-run.sh
+++ demo-app/scripts/ci/detox-ios-run.sh
@@ -9,6 +9,20 @@ rm -f ./reports/e2e-tests/ios-e2e-tests.html
 
 ./scripts/start-react-native-background.sh
 
+echo "Deleting any detox lock file"
+filePathToDelete=~/.local/share/Detox/device.registry.state.lock
+if test -f "${filePathToDelete}"; then
+    echo "Deleting ${filePathToDelete}"
+    rm -f $filePathToDelete
+fi
+
+filePathToDelete=~/Library/Detox/device.registry.state.lock
+if test -f "${filePathToDelete}"; then
+    echo "Deleting ${filePathToDelete}"
+    rm -f $filePathToDelete
+fi
+
+
 echo "Running Detox tests for iOS"
 JEST_HTML_REPORTER_OUTPUT_PATH="./reports/e2e-tests/ios-e2e-tests.html" detox test --configuration ios-ci --cleanup --loglevel trace
 status=$?
