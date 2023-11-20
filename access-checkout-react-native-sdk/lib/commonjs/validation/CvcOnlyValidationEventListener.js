993ab7b42a3a0870ac3e6d6cbfd79343930125ae
Jason Dzela-Mensah <jason.dzelamensah@fisglobal.com>
2023-11-20 11:13:18 +0000

    US1762570: update package.json and related files in demo-app & sdk

--- access-checkout-react-native-sdk/lib/commonjs/validation/CvcOnlyValidationEventListener.js
+++ access-checkout-react-native-sdk/lib/commonjs/validation/CvcOnlyValidationEventListener.js
@@ -4,14 +4,12 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.cvcOnlyValidationNativeEventListenerOf = cvcOnlyValidationNativeEventListenerOf;
-
 function cvcOnlyValidationNativeEventListenerOf(delegate) {
   // eslint-disable-next-line  @typescript-eslint/no-explicit-any
   return function (event) {
     if (event.type === 'cvc' && delegate.onCvcValidChanged) {
       delegate.onCvcValidChanged(event.isValid);
     }
-
     if (event.type === 'all' && delegate.onValidationSuccess) {
       delegate.onValidationSuccess();
     }
--- access-checkout-react-native-sdk/lib/commonjs/validation/CvcOnlyValidationHooks.js
+++ access-checkout-react-native-sdk/lib/commonjs/validation/CvcOnlyValidationHooks.js
@@ -5,16 +5,13 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.useCvcOnlyValidation = useCvcOnlyValidation;
 exports.useCvcOnlyValidationEventListener = useCvcOnlyValidationEventListener;
-
 var _react = require("react");
-
 var _reactNative = require("react-native");
-
 var _index = _interopRequireWildcard(require("../index"));
-
-function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
-
-function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function (e) { return e ? t : r; })(e); }
+function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != typeof e && "function" != typeof e) return { default: e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && Object.prototype.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n.default = e, t && t.set(e, n), n; }
+// eslint-disable-next-line  @typescript-eslint/ban-ts-comment
+// @ts-ignore
 
 function useCvcOnlyValidationEventListener(merchantListener) {
   (0, _react.useEffect)(() => {
@@ -26,14 +23,11 @@ function useCvcOnlyValidationEventListener(merchantListener) {
     };
   }, []);
 }
-
 function useCvcOnlyValidation(accessCheckout, cvcOnlyValidationConfig, merchantListener) {
   useCvcOnlyValidationEventListener(merchantListener);
-
   const initialiseCvcOnlyValidation = () => {
     return accessCheckout.initialiseCvcOnlyValidation(cvcOnlyValidationConfig);
   };
-
   return {
     initialiseCvcOnlyValidation
   };
