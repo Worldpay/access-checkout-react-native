a4053f41c088c8561d15473a1b4cdbe9cfa90492
Jason Dzela-Mensah <jason.dzelamensah@fisglobal.com>
2023-11-23 11:00:26 +0000

    US1762570: attempt fix for ios bridge tests

--- access-checkout-react-native-sdk/ios/AccessCheckoutReactNativeUnitTests/AccessCheckoutReactNativeCardValidationAcceptanceTests.swift
+++ access-checkout-react-native-sdk/ios/AccessCheckoutReactNativeUnitTests/AccessCheckoutReactNativeCardValidationAcceptanceTests.swift
@@ -486,7 +486,7 @@ class AccessCheckoutReactNativeCardValidationAcceptanceTests: XCTestCase {
     private class AccessCheckoutReactNativeTestImplementation: AccessCheckoutReactNative {
         private(set) var eventsSent: [RCTEventMock] = []
 
-        override func sendEvent(withName name: String!, body: Any!) {
+         func sendEventWithName(withName name: String!, body: Any!) {
             let eventMock = RCTEventMock(name, bodyDictionary: body as! NSDictionary)
             eventsSent.append(eventMock)
         }
