4dcc5cc1f96a9406c43b9915104bb23090b54309
e5697436 <153538325+abaeza-wp@users.noreply.github.com>
2024-01-04 16:06:39 +0000

    US1762585: Update e2e tests

---
+++ demo-app/e2e/page-objects/AccessCheckoutTextInputPO.js
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { UIComponentPO } = require('./UIComponentPO');
+const { expect, element, by } = require('detox');
+/* eslint-enable @typescript-eslint/no-var-requires */
+
+class AccessCheckoutTextInputPO extends UIComponentPO {
+  component() {
+    return element(by.id('.*').withAncestor(this.id));
+  }
+
+  async type(text, expectedText = '') {
+    await this.component().typeText(text);
+
+    if (expectedText) {
+      await expect(this.component()).toHaveText(expectedText);
+    }
+  }
+
+  async text() {
+    const attributes = await this.getAttributes();
+    return attributes.text;
+  }
+}
+
+module.exports = { AccessCheckoutTextInputPO };
