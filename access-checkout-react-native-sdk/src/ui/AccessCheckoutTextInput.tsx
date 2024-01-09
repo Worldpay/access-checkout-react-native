6586dd392499346e5a746f2ef8d9e40dc0afa272
e5697436 <153538325+abaeza-wp@users.noreply.github.com>
2024-01-09 12:49:28 +0000

    US1762588: Add Android AccessCheckoutEditText defaults when initialising the component

--- access-checkout-react-native-sdk/src/ui/AccessCheckoutTextInput.tsx
+++ access-checkout-react-native-sdk/src/ui/AccessCheckoutTextInput.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {
   requireNativeComponent,
-  StyleProp,
-  TextStyle,
-  type ViewProps,
+  type StyleProp,
+  type TextStyle,
+  View,
+  type ViewStyle,
 } from 'react-native';
+import type { ColorValue } from 'react-native/Libraries/StyleSheet/StyleSheet';
 
 /**
  * Composes `AccessCheckoutTextInput`.
@@ -15,21 +17,57 @@ import {
  * - editable: boolean
  * - placeholder: string
  */
-interface AccessCheckoutTextInputProps extends ViewProps {
-  testID: string | undefined;
-  style?: StyleProp<TextStyle>;
-  editable?: boolean | undefined;
-  placeholder?: string | undefined;
-  isValid?: boolean;
+interface AccessCheckoutTextInputProps {
+  nativeID: string;
+  testID?: string;
+  style?: StyleProp<ViewStyle>;
+  placeholder?: string;
   keyboardType?: string;
+  textColor?: ColorValue;
+  font?: string;
+  editable?: boolean;
+}
+
+interface RTCAccessCheckoutTextInputProps {
+  nativeID: string;
+  testID?: string;
+  style?: StyleProp<TextStyle>;
+  placeholder?: string;
+  keyboardType?: string; //maybe remove?
+  textColor?: ColorValue;
+  font?: string;
+  editable?: boolean;
 }
 
 const RTCAccessCheckoutTextInput =
-  requireNativeComponent<AccessCheckoutTextInputProps>(
+  requireNativeComponent<RTCAccessCheckoutTextInputProps>(
     'AccessCheckoutTextInput'
   );
 const AccessCheckoutTextInput = (props: AccessCheckoutTextInputProps) => {
-  return <RTCAccessCheckoutTextInput {...props} />;
+  const {
+    nativeID,
+    testID,
+    style,
+    placeholder,
+    keyboardType,
+    textColor,
+    editable,
+  } = props;
+
+  console.log(placeholder);
+  return (
+    <View style={[style]}>
+      <RTCAccessCheckoutTextInput
+        nativeID={nativeID}
+        testID={testID}
+        style={[{ flex: 1 }]}
+        placeholder={placeholder}
+        keyboardType={keyboardType}
+        textColor={textColor}
+        editable={editable}
+      />
+    </View>
+  );
 };
 
 export default AccessCheckoutTextInput;
