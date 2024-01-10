fc5713c844468bfa2b60e1bb9e111e5ff6b5225e
e5697436 <153538325+abaeza-wp@users.noreply.github.com>
2024-01-10 10:22:26 +0000

    US1762588: Revert pod local version

--- access-checkout-react-native-sdk/ios/AccessCheckoutReactNative/AccessCheckoutTextInputManager.m
+++ access-checkout-react-native-sdk/ios/AccessCheckoutReactNative/AccessCheckoutTextInputManager.m
@@ -9,9 +9,7 @@
 RCT_REMAP_VIEW_PROPERTY(placeholder, placeholder, NSString)
 RCT_REMAP_VIEW_PROPERTY(editable, enabled, BOOL)
 
-// The following properties use RCTConvert behind the scenes to convert to appropriate types
 RCT_REMAP_VIEW_PROPERTY(textColor, textColor, UIColor)
 RCT_REMAP_VIEW_PROPERTY(font, font, UIFont)
-//RCT_REMAP_VIEW_PROPERTY(keyboardType, keyboardType, UIKeyboardType)
 
 @end
