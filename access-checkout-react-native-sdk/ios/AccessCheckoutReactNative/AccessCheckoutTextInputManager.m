441e163fc66780ed2da9acc1a9be40f02f768b88
abaeza-wp <153538325+abaeza-wp@users.noreply.github.com>
2024-01-15 08:20:58 +0000

    US1762588 - [iOS] React Native Demo Components Refactor and Props (#86)
    * Us1762585 - iOS Refactor Native components (#81)
    
    * US1762570: create view manager to native AccessCheckoutEditText component; override onTextChange to support the react native onChangeText event; implement component in react native
    
    * US1762550: update card details field to use new component; update RN module to use reference to AccessCheckoutEditText and not the text value
    
    * US1762550: move AccessCheckoutEditTextManager to correct file path; update local android sdk files to most up-to-date version of master; update AccessCheckoutReactPackageTest
    
    * US1782181: add unit tests for AccessCheckoutEditTextManagerTest; add mockito to build.gradle for unit tests
    
    * US1762551: Expose AccessCheckoutUITextField to be able to use native components via the ios Bridge
    Simplify ReactNativeViewLocator and implement/expose specific POC methods to avoid disruptions with previous implementation
    Add POC toggle to demo app to be able to switch implementations
    Expose new POC validation and sessionGeneration methods to avoid diruptions with previous implementations
    
    * US1762551: Point client to npe environment
    
    * US1762585: Refactor ios Bridge and demo application
    Avoids having to handle inputs in states
    Uses native components
    Only a input field id is needed in order to configure validation and session generation for a field
    Integrate the latest AccessCheckoutUITextField changes
    Updates and renames cvcValue to cvcId
    Updates and renames panValue to panId
    Updates and renames expiryDateValue to expiryDateId
    
    * US1762585: Rebuild lib and clean up files
    
    * US1762585: Minor cleanup
    
    * US1762585: Rebuild lib files
    Rename AccessCheckoutEditText to AccessCheckoutInputText as EditText was specific to Android
    Minor renaming of fields like panUITextField to panACUITextField to improve readability and make it known that we are dealing with AccessCheckoutUITextField in the iOS bridge
    
    * US1762585:
    Rename AccessCheckoutEditText to AccessCheckoutInputText in Android bridge
    
    * US1762585:
    Rename AccessCheckoutInputText to AccessCheckoutTextInput
    Rebuild lib files
    
    * US1762585: Update bitrise-step to use xcode-test v5 and update e2e tests (#82)
    
    * US1762585: Update bitrise-step to use xcode-test v5
    
    * US1762585: Use Optionals to mantain compatibility in both Xcode 13 and Xcode 15
    
    * US1762585: Update e2e tests
    
    * US1762585: Improve matcher based on platform using type
    
    * US1762585: Remove TextInputPO as it is no longer used
    
    ---------
    
    Co-authored-by: e5661323 <jason.dzelamensah@fisglobal.com>
    
    * US1762588: Clean and Refactor CVC Expiry and Pan React Native Demo components
    Define Props and types to ReactNative component and Native component props
    
    * US1762588: Use paddingHorizontal in demo components
    
    * US1762588: Add Android AccessCheckoutEditText defaults when initialising the component
    
    * US1762588: Add Support for fonts via styling, minor refactorings
    
    * US1762588: Remove Comment and support for keyboartType in iOS Bridge
    
    * US1762588: Revert pod local version
    
    * US1762588: rebuild pod files to point to remote AccessCheckoutSDK
    
    * US1762588: rebuild pod files to point to remote AccessCheckoutSDK
    
    * US1762588: Remove support for fontStyle and fontWeight
    
    * US1762588: Add correct colours to text when input is not editable, remove inverted expression
    
    * US1762588: Fixx import after rebase
    
    * US1762588: Clean imports
    
    * US1762588: Remove unnecessary comments
    
    * US1762588: Rename fontColor to color, and remap in iOS Bridge
    Reset defaults provided by AccessCheckoutUITextfield
    Reset insets using horizontalPadding in AccessCheckoutUITextfield
    
    * US1762588: point to correct tag instad of branch
    
    * US1762588: Update pod deps
    
    * US1762588: change BitRise flow for iOS Bridge and iOS e2e tests to update and install Pods rather than just install
    
    ---------
    
    Co-authored-by: e5661323 <jason.dzelamensah@fisglobal.com>
    Co-authored-by: Olivier Chalet <olivier.chalet@fisglobal.com>

--- access-checkout-react-native-sdk/ios/AccessCheckoutReactNative/AccessCheckoutTextInputManager.m
+++ access-checkout-react-native-sdk/ios/AccessCheckoutReactNative/AccessCheckoutTextInputManager.m
@@ -2,6 +2,14 @@
 #import <React/RCTViewManager.h>
 
 @interface RCT_EXTERN_MODULE(AccessCheckoutTextInputManager, RCTViewManager)
-//Externalises properties
-RCT_EXPORT_VIEW_PROPERTY(placeholder, NSString)
+
+//Externalises properties using a remap in case of name changes
+//RCT_REMAP_VIEW_PROPERTY(RN property name, ios property name, type)
+
+RCT_REMAP_VIEW_PROPERTY(placeholder, placeholder, NSString)
+RCT_REMAP_VIEW_PROPERTY(editable, isEnabled, BOOL)
+
+RCT_REMAP_VIEW_PROPERTY(color, textColor, UIColor)
+RCT_REMAP_VIEW_PROPERTY(font, font, UIFont)
+
 @end
--- demo-app/src/common/ExpiryDateField.tsx
+++ demo-app/src/common/ExpiryDateField.tsx
@@ -1,44 +1,42 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import commonStyles from './common-styles.js';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import UIComponentProps from './UIComponentProps';
-import AccessCheckoutTextInput from '../../../access-checkout-react-native-sdk/src/ui/AccessCheckoutTextInput';
 
-const styles = StyleSheet.create({
-  expiry: {
-    flex: 1,
-    margin: 12,
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 10,
-    height: 40,
-  },
-});
+import type UIComponentProps from './UIComponentProps';
+import AccessCheckoutTextInput from '../../../access-checkout-react-native-sdk/src/ui/AccessCheckoutTextInput';
+import { StyleSheet } from 'react-native';
 
 interface ExpiryDateFieldProps extends UIComponentProps {
   isEditable: boolean;
   isValid: boolean;
 }
 
+const styles = StyleSheet.create({
+  expiry: {
+    flex: 1,
+    flexDirection: 'row',
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 5,
+    height: 40,
+    paddingHorizontal: 10,
+  },
+});
+
 const ExpiryDateField = (props: ExpiryDateFieldProps) => {
+  const validationColours = props.isValid ? 'green' : 'red';
+  const validationColourStyle = props.isEditable ? validationColours : 'silver';
+
   return (
     <AccessCheckoutTextInput
       nativeID="expiryDateInput"
       testID={props.testID}
-      keyboardType="numeric"
       editable={props.isEditable}
       placeholder="MM/YY"
       style={[
         styles.expiry,
-        !props.isEditable
-          ? commonStyles.greyedOut
-          : props.isValid
-          ? commonStyles.valid
-          : commonStyles.invalid,
+        {
+          color: validationColourStyle,
+          borderColor: validationColourStyle,
+        },
       ]}
     />
   );
