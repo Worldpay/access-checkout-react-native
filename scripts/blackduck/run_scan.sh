2379fbcc595bef6c2f1a34a5d5dcd02cbfababa9
Olivier Chalet <olivier.chalet@fisglobal.com>
2024-01-17 16:07:51 +0000

    NOTICKET: fix Blackduck job issues, see note below - the job used to complete successfully despite some of the Blackduck scans failing. The issue has been fixed by adding some statements to check the exit code of each command used to run Blackduck scans - the Blackduck scans for the Android bridge used to be empty. This was due to the fact that the Android Gradle configurations (i.e. artifacts configurations names) had changed following to the React Native migration to 0.70.0 but these had not been updated in the Blackduck scan, hence the old config specified could not be found and Blackduck would produce an empty report

--- scripts/blackduck/run_scan.sh
+++ scripts/blackduck/run_scan.sh
@@ -4,7 +4,7 @@ echo "Initiating Blackduck Scan..."
 cd access-checkout-react-native-sdk
 SDK_VERSION=$(sed -e 's/^"//' -e 's/"$//' <<< $(jq '.version' package.json))
 ANDROID_BRIDGE_VERSION=$(cat android/access-checkout-react-native-sdk-android-bridge/gradle.properties | grep -m 1 'version=' | sed 's/version=//')
-ANDROID_BRIDGE_GRADLE_CONFIGURATIONS_TO_SCAN="mainReleaseCompileClasspath,mainReleaseRuntimeClasspath"
+ANDROID_BRIDGE_GRADLE_CONFIGURATIONS_TO_SCAN="coreReleaseCompileClasspath,coreReleaseRuntimeClasspath"
 IOS_BRIDGE_VERSION=$(cat ios/AccessCheckoutReactNativeSDKiOSBridge.podspec | grep -m 1 's.version' | sed -e 's/\ //g' -e 's/s\.version=//' -e 's/\"//g')
 
 if [ -z "${SDK_VERSION}" ]
@@ -40,5 +40,22 @@ then
 fi
 
 ./detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME_RN --detect.project.version.name=$SDK_VERSION --detect.risk.report.pdf=true --detect.npm.include.dev.dependencies=false --detect.excluded.directories=android,ios
-./detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME_ANDROID_BRIDGE --detect.project.version.name=$ANDROID_BRIDGE_VERSION --detect.risk.report.pdf=true --detect.source.path=android --detect.gradle.included.configurations="$ANDROID_BRIDGE_GRADLE_CONFIGURATIONS_TO_SCAN"
-./detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME_IOS_BRIDGE --detect.project.version.name=$IOS_BRIDGE_VERSION --detect.risk.report.pdf=true --detect.source.path=ios
+if [ $? -ne 0 ] ; then
+  echo "Failed scan for TypeScript SDK. Exiting"
+  exit 1
+fi
+
+# Android SDK
+cd android
+../detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME_ANDROID_BRIDGE --detect.project.version.name=$ANDROID_BRIDGE_VERSION --detect.risk.report.pdf=true --detect.gradle.included.configurations="$ANDROID_BRIDGE_GRADLE_CONFIGURATIONS_TO_SCAN"
+if [ $? -ne 0 ] ; then
+  echo "Failed scan for Android bridge. Exiting"
+  exit 1
+fi
+
+cd ../ios
+../detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME_IOS_BRIDGE --detect.project.version.name=$IOS_BRIDGE_VERSION --detect.risk.report.pdf=true
+if [ $? -ne 0 ] ; then
+  echo "Failed scan for iOS bridge. Exiting"
+  exit 1
+fi
