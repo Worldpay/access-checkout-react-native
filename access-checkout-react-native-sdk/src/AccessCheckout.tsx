3e23d96f21532ecfb3347b6944fb7cac60ec7333
Olivier Chalet <olivier.chalet@fisglobal.com>
2024-01-19 10:30:21 +0000

    US1838743: bump version to 2.0.1

--- access-checkout-react-native-sdk/src/AccessCheckout.tsx
+++ access-checkout-react-native-sdk/src/AccessCheckout.tsx
@@ -13,7 +13,7 @@ import CardValidationConfig from './validation/CardValidationConfig';
 import CvcOnlyValidationConfig from './validation/CvcOnlyValidationConfig';
 
 export default class AccessCheckout {
-  private readonly ReactNativeSdkVersion = '2.0.0';
+  private readonly ReactNativeSdkVersion = '2.0.1';
   static readonly CardValidationEventType = 'AccessCheckoutCardValidationEvent';
   static readonly CvcOnlyValidationEventType =
     'AccessCheckoutCvcOnlyValidationEvent';
