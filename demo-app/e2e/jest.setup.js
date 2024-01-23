bedec09ebfa83ee96034fc8d3e78664701c243f8
abaeza-wp <153538325+abaeza-wp@users.noreply.github.com>
2024-01-23 13:30:44 +0000

    NOTICKET: Improve end to end tests using retries for a more reliable run in ci pipelines and ensuring labels exists before extracting attributes (#98)

---
+++ demo-app/e2e/jest.setup.js
@@ -0,0 +1,8 @@
+// Attempt to re-run individual tests when they fail
+const retries = 5;
+console.log('==========================');
+console.log(
+  `Running Jest with retries: will retry failing tests ${retries} time(s).`
+);
+console.log('==========================');
+jest.retryTimes(retries);
