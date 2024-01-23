503d24795f2d033af42c5c9f2c76c1ee6737c42f
abaeza-wp <153538325+abaeza-wp@users.noreply.github.com>
2024-01-23 10:29:48 +0000

    Us1833001: [Android]  Add font support (#96)
    * US1833001: Add Android support for font customization including font weight and font style
    
    * US1833001: Add Android support for font customization including font weight and font style and checks for support of font weight in android api >28
    
    * US1833001: Better comment to explain why the use of font.getInt() was not used

--- access-checkout-react-native-sdk/android/access-checkout-react-native-sdk-android-bridge/src/main/java/com/worldpay/access/checkout/reactnative/ui/AccessCheckoutTextInputManager.kt
+++ access-checkout-react-native-sdk/android/access-checkout-react-native-sdk-android-bridge/src/main/java/com/worldpay/access/checkout/reactnative/ui/AccessCheckoutTextInputManager.kt
@@ -1,6 +1,7 @@
 package com.worldpay.access.checkout.reactnative.ui
 
 import android.graphics.Typeface
+import android.os.Build
 import android.view.Gravity
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReadableMap
@@ -9,6 +10,7 @@ import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.ViewProps
 import com.facebook.react.uimanager.annotations.ReactProp
 import com.worldpay.access.checkout.ui.AccessCheckoutEditText
+import java.lang.reflect.Type
 
 
 class AccessCheckoutTextInputManager(private val callerContext: ReactApplicationContext) :
@@ -41,18 +43,56 @@ class AccessCheckoutTextInputManager(private val callerContext: ReactApplication
         accessCheckoutEditText.setTextColor(color)
     }
 
+
     @ReactProp(name = "font")
     fun setRTCFont(accessCheckoutEditText: AccessCheckoutEditText, font: ReadableMap) {
+        var customTypeface = Typeface.DEFAULT
+        var fontWeightProvidedAsUnit = false;
+
         if (font.hasKey(ViewProps.FONT_SIZE)) {
-            val fontSize = font.getDouble(ViewProps.FONT_SIZE)
-            accessCheckoutEditText.textSize = fontSize.toFloat()
+            val fontSize: Float = font.getDouble(ViewProps.FONT_SIZE).toFloat()
+            accessCheckoutEditText.textSize = fontSize
         }
 
-        if (font.hasKey(ViewProps.FONT_FAMILY)) {
-            val fontSize = font.getString(ViewProps.FONT_FAMILY)
-            val typeface = Typeface.create(fontSize, Typeface.NORMAL)
-            accessCheckoutEditText.typeface = typeface
+        if (font.hasKey(ViewProps.FONT_WEIGHT)) {
+            fontWeightProvidedAsUnit = when (font.getString(ViewProps.FONT_WEIGHT)) {
+                "bold", "normal" -> false
+                else -> true
+            }
         }
+
+        val bold = isBold(font)
+        val italic = isItalic(font)
+
+        val fontStyle: Int = when {
+            bold && italic -> Typeface.BOLD_ITALIC
+            bold -> Typeface.BOLD
+            italic -> Typeface.ITALIC
+            else -> Typeface.NORMAL
+        }
+
+        // Always attempt to get Font family if the font family is null or unsupported, a default one will be used
+        customTypeface = Typeface.create(font.getString(ViewProps.FONT_FAMILY), fontStyle)
+
+        // Font Weight only supported in API >28
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P && fontWeightProvidedAsUnit) {
+            val fontAsString = font.getString(ViewProps.FONT_WEIGHT)
+            // Note: This default should never happen as we use fontWeightProvidedAsUnit as a check
+            // but to avoid a compilation false positive we default to 400 which should be the regular for fonts.
+            // Also: Using font.getInt() seems to cause a casting issue where it casts tries to cast a String to a Double, which makes the app crash.
+            val fontWeightNumber = fontAsString?.toInt() ?: 400
+            customTypeface = Typeface.create(customTypeface, fontWeightNumber, italic)
+        }
+
+        accessCheckoutEditText.typeface = customTypeface;
+    }
+
+    private fun isItalic(font: ReadableMap): Boolean {
+        return font.hasKey(ViewProps.FONT_STYLE) && "italic" == font.getString(ViewProps.FONT_STYLE)
+    }
+
+    private fun isBold(font: ReadableMap): Boolean {
+        return font.hasKey(ViewProps.FONT_WEIGHT) && "bold" == font.getString(ViewProps.FONT_WEIGHT)
     }
 
     @ReactProp(name = "placeholder")
@@ -64,4 +104,6 @@ class AccessCheckoutTextInputManager(private val callerContext: ReactApplication
     fun setRTCEditable(accessCheckoutEditText: AccessCheckoutEditText, editable: Boolean) {
         accessCheckoutEditText.isEnabled = editable
     }
+
 }
+
