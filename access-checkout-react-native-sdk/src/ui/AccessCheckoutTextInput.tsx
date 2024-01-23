3a3256ba92f52d02e8232beab3f3ebe28b9c41a6
abaeza-wp <153538325+abaeza-wp@users.noreply.github.com>
2024-01-23 10:29:48 +0000

    Us1833001: [Android]  Add font support (#96)
    * US1833001: Add Android support for font customization including font weight and font style
    
    * US1833001: Add Android support for font customization including font weight and font style and checks for support of font weight in android api >28
    
    * US1833001: Better comment to explain why the use of font.getInt() was not used

--- access-checkout-react-native-sdk/src/ui/AccessCheckoutTextInput.tsx
+++ access-checkout-react-native-sdk/src/ui/AccessCheckoutTextInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { requireNativeComponent, type StyleProp, StyleSheet, View, type ViewStyle } from 'react-native';
+import { requireNativeComponent, type StyleProp, StyleSheet, type TextStyle, View, type ViewStyle } from 'react-native';
 import type { ColorValue } from 'react-native/Libraries/StyleSheet/StyleSheet';
 
 /**
@@ -20,29 +20,41 @@ interface AccessCheckoutTextInputProps {
 }
 
 /**
- * Note: Not all properties apply styling to placeholder text and input text.
+ * Note: Not all properties apply styling to both placeholder text and input text.
  *   textColor: only applies to input text.
  *   fontFamily: applies to both placeholder text and input text.
- *   fontSize:applies to both placeholder text and input text.
+ *   fontSize: applies to both placeholder text and input text.
+ *   fontStyle: applies to both placeholder text and input text.
+ *   fontWeight: applies to both placeholder text and input text.
  */
 interface AccessCheckoutTextInputStyle extends ViewStyle {
   color?: ColorValue;
   fontFamily?: string;
   fontSize?: number;
+  fontStyle?: 'normal' | 'italic';
+  // Specifies font weight. The values 'normal' and 'bold' are supported for most fonts. Not all fonts have a variant for each of the numeric values, in that case the closest one is chosen.
+  fontWeight?: 'normal' | 'bold' | '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900';
 }
 
 /**
  * Font Changes apply to placeholder text and input text
+ *
+ * ## What
+ * - Rewrites font support for android
+ * - Adds font weight support for android <28 only 'normal' and 'bold' supported
+ * - Adds font weight support as units for android >28
  */
 interface RTCAccessCheckoutTextInputFontProps {
   fontFamily?: string;
   fontSize?: number;
+  fontStyle?: 'normal' | 'italic';
+  fontWeight?: 'normal' | 'bold' | '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900';
 }
 
 interface RTCAccessCheckoutTextInputProps {
   nativeID: string;
   testID?: string;
-  style?: StyleProp<ViewStyle>;
+  style?: StyleProp<TextStyle>;
   placeholder?: string;
   font?: RTCAccessCheckoutTextInputFontProps;
   editable?: boolean;
@@ -52,7 +64,7 @@ interface RTCAccessCheckoutTextInputProps {
 const RTCAccessCheckoutTextInput = requireNativeComponent<RTCAccessCheckoutTextInputProps>('AccessCheckoutTextInput');
 const AccessCheckoutTextInput = (props: AccessCheckoutTextInputProps) => {
   const { nativeID, testID, style, placeholder, editable } = props;
-  const { color, fontFamily, fontSize, ...otherStyles } = StyleSheet.flatten([style]);
+  const { color, fontFamily, fontSize, fontStyle, fontWeight, ...otherStyles } = StyleSheet.flatten([style]);
   return (
     <View style={[otherStyles]}>
       <RTCAccessCheckoutTextInput
@@ -63,6 +75,8 @@ const AccessCheckoutTextInput = (props: AccessCheckoutTextInputProps) => {
         font={{
           fontFamily,
           fontSize,
+          fontWeight,
+          fontStyle,
         }}
         color={color}
         editable={editable}
