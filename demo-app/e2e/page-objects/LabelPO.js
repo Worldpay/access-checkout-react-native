5209c68ece3e005c070a0206261411a490bd8814
e5697436 <153538325+abaeza-wp@users.noreply.github.com>
2024-01-23 10:38:57 +0000

    Improve end to end tests using retries for a more reliable run in ci pipelines and ensuring labels exists before extracting attributes

--- demo-app/e2e/page-objects/LabelPO.js
+++ demo-app/e2e/page-objects/LabelPO.js
@@ -1,9 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { UIComponentPO } = require('./UIComponentPO');
+const { waitFor } = require('detox');
+const { TIMEOUT_IN_MS } = require('./Options');
+
 /* eslint-enable @typescript-eslint/no-var-requires */
 
 class LabelPO extends UIComponentPO {
   async text() {
+    await waitFor(this.component()).toExist().withTimeout(TIMEOUT_IN_MS);
     const attributes = await this.getAttributes();
     return attributes.text;
   }
