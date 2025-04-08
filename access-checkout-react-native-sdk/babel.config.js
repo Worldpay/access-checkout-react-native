6e309024668e41699bca3bbc49d2e12fc03e364f
Alejandro Baeza Gutierrez <alejandro.baezagutierrez@worldpay.com>
2025-04-08 08:08:17 +0100

    US2097609: Upgrade react-native to 0.73: Fix babel dep

--- access-checkout-react-native-sdk/babel.config.js
+++ access-checkout-react-native-sdk/babel.config.js
@@ -1,8 +1,3 @@
-// eslint-disable-next-line no-undef
 module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-typescript',
-    'module:metro-react-native-babel-preset',
-  ],
+  presets: ['module:@react-native/babel-preset'],
 };
